6ba0442cb8f387a1414b29d0d259a1cdafdced57
David Mateu <davidmateupascual@gmail.com>
2015-06-08 14:48:53 +0200

    Edición

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -50,7 +50,7 @@ exports.new = function(req, res){
 	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
-//POST /quiz/create
+//POST /quizes/create
 exports.create = function(req, res){
 	var quiz = models.Quiz.build(req.body.quiz);
 
@@ -65,4 +65,35 @@ exports.create = function(req, res){
 			}
 		}
 	);
+};
+
+
+//GET /quizes/:quizId/edit
+exports.edit = function(req, res){
+	var quiz = req.quiz
+ 	
+ 	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+//PUT /quizes/:quizId
+exports.update = function(req, res){
+
+	req.quiz.pregunta = req.body.quiz.pregunta;	
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(
+		function(err){
+			if (err){
+				res.render('quizes/edit',{quiz: req.quiz, errors: err.errors});
+			}else{
+				req.quiz
+				.save({fields: ["pregunta", "respuesta"]}).then(function(){
+					res.redirect('/quizes');
+				})	//Redireccion HTTP (URL relativo) lista de preguntas
+			}
+		}
+	);	
+
 };
--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,14 @@ router.get('/', function(req, res) {
 router.param('quizId',quizController.load); 
 
 
-/* GET quizes */
+/* RUTAS quizes */
 router.get('/quizes',					   quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',				   quizController.new);
-/* POST quizes */
 router.post('/quizes/create',			   quizController.create);
-
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
 
 /* GET author */
 router.get('/author', function(req, res) {
